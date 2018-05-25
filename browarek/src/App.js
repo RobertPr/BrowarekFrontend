d245ca40111d1c28fe3dd082988da2199130f54f
RobertPr <protasiukrobert@wp.pl>
2018-05-25 16:45:09 +0200

    dobra nie ruszam juz tego gita, jest stabilnie

--- browarek/src/App.js
+++ browarek/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter, Router } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Login from './components/login/Login';
 
 class App extends Component {
