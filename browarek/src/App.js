5203a9e586bec966f552dfd30f8ea31cee946b3b
RobertPr <protasiukrobert@wp.pl>
2018-05-26 13:43:05 +0200

    ekran dodawania piwa

--- browarek/src/App.js
+++ browarek/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import AddBeer from './components/addBeer/AddBeer';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <Switch>
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      <Route exact path="/addBeer" component={AddBeer} />
       </Switch>
     );
   }
---
+++ browarek/src/components/addBeer/AddBeer.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import './AddBeer.css';
+import { Link } from 'react-router-dom';
+
+class AddBeer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { error: "" };
+    }
+
+    render = () => {
+        return (
+            <React.Fragment>
+                <div id="addBeerPhoto">
+                    <span id="addImgDesc">Kliknij, aby dodać zdjęcie piwa</span>
+                </div>
+                <div id="addBreweryPhoto">
+                    <span id="addImgDesc">Kliknij, aby dodać zdjęcie browaru</span>
+                </div>
+                <div id="addBreweryContainer">
+                    <div id="addBreweryTitle">Dodaj</div>
+                    <div id="searchSwitch">
+                        <button id="beerSwitch">Piwo</button>
+                        <button id="brewerySwitch">Browar</button>
+                    </div>
+                    <form id="beerForm">
+                        <div>
+                            <label>Styl</label>
+                            <input className="addBreweryInput" type="text" name="Style" />
+                        </div>
+                        <div>
+                            <label>Obj. alkoholu w %</label>
+                            <input className="addBreweryInput" type="text" name="alco%" />
+                        </div>
+                        <div>
+                            <label>Gorzkość</label>
+                            <input className="addBreweryInput" type="text" name="Gorzkosc" />
+                        </div>
+                        <div>
+                            <label>Ekstrakt w *Blg</label>
+                            <input className="addBreweryInput" type="text" name="Ekstrakt" />
+                        </div>
+                        <div id="rateBeerStars">
+                            <label className="rateBeerTitle">Twoja ocena</label>
+                            <div className="rating">
+                                <input id="star5" name="star" type="radio" value="5" className="radio-btn hide" />
+                                <label className="starButtons" for="star5">☆</label>
+                                <input id="star4" name="star" type="radio" value="4" className="radio-btn hide" />
+                                <label className="starButtons" for="star4">☆</label>
+                                <input id="star3" name="star" type="radio" value="3" className="radio-btn hide" />
+                                <label className="starButtons" for="star3">☆</label>
+                                <input id="star2" name="star" type="radio" value="2" className="radio-btn hide" />
+                                <label className="starButtons" for="star2">☆</label>
+                                <input id="star1" name="star" type="radio" value="1" className="radio-btn hide" />
+                                <label className="starButtons" for="star1">☆</label>
+                                <div className="clear"></div>
+                            </div>
+                        </div>
+                        <textarea className="addBreweryInputLong" type="text" name="Comment" placeholder="Komentarz"></textarea>
+                        <br></br>
+                        <textarea className="addBreweryInputLong" type="text" name="beerDesc" placeholder="Opis piwa"></textarea>
+                            <div id="checkboxDiv">
+                                <input type="checkbox" name="addBrowarCheckBox" value="checked" />
+                                <label> Dodaj nowy browar</label>
+                                <select className="addBreweryInput" name="addBreweryList">
+                                <option hidden>Wybierz browar z listy</option>
+                                </select>
+			                </div>
+                        <div>
+                            <label>Nazwa browaru</label>
+                            <input className="addBreweryInput" type="text" name="alco%" />
+                        </div>
+                        <div>
+                            <label>Typ producenta</label>
+                            <input className="addBreweryInput" type="text" name="producerType" />
+                        </div>
+                        <div>
+                            <label>Kraj</label>
+                            <input className="addBreweryInput" type="text" name="Country" />
+                        </div>
+                        <div>
+                            <label>Rok założenia</label>
+                            <input className="addBreweryInput" type="text" name="creationYear" />
+                        </div>
+                        <input name="addButton" type="submit" value="Dodaj" />
+                    </form>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export default AddBeer;
