1e7db7ecabfed5ef64a61395a45a7e04b6fba313
kpietraszko <krzysztof@pietraszko.cba.pl>
2018-05-27 15:25:39 +0200

    poczatek logowania

--- browarek/src/App.js
+++ browarek/src/App.js
@@ -15,25 +15,28 @@ import NavbarUnLogged from './components/navbarUnlogged/NavbarUnLogged';
 import MainScreen from './components/mainScreen/MainScreen';
 
 class App extends Component {
+  state = {}
+  onLogin = (token, userId) => {
+    this.setState({ token, userId });
+  }
   render() {
     return (
       <React.Fragment>
-        {/* {props.logged} ? <NavbarLogged /> : <NavbarUnLogged />  zmienic na to jak bedzie wiadomo co z logowaniem */}
-        <NavbarLogged />
+        {this.state.token ? <NavbarLogged /> : <NavbarUnLogged /> }
         <Switch>
-          <Route exact path="/home" component={MainScreen} />
-          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={MainScreen} />
+          <Route exact path="/login" component={Login} onLogin={this.onLogin} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/addBeer" component={AddBeer} />
           <Route exact path="/addBrewery" component={AddBrewery} />
           <Route exact path="/breweryCard" component={BreweryCard} /> {/* zmienic zeby bylo id browaru */}
-          <Route exact path="/beerCard" component={BeerCard}/> {/* zmienic zeby bylo id piwa */}
-          <Route exact path="/beerSearch" component={BeerSearch}/>
-          <Route exact path="/brewerySearch" component={BrewerySearch}/>
-          <Route exact path="/beerResults" component={BeerResults}/>
-          <Route exact path="/breweryResults" component={BreweryResults}/>
-          <Route exact path="/nav" component={NavbarLogged}/> {/* test only */}
-          <Route exact path="/navun" component={NavbarUnLogged}/> {/* test only */}
+          <Route exact path="/beerCard" component={BeerCard} /> {/* zmienic zeby bylo id piwa */}
+          <Route exact path="/beerSearch" component={BeerSearch} />
+          <Route exact path="/brewerySearch" component={BrewerySearch} />
+          <Route exact path="/beerResults" component={BeerResults} />
+          <Route exact path="/breweryResults" component={BreweryResults} />
+          <Route exact path="/nav" component={NavbarLogged} /> {/* test only */}
+          <Route exact path="/navun" component={NavbarUnLogged} /> {/* test only */}
         </Switch>
       </React.Fragment>
     );
