6c9fb91322accb65583ab3294b5dd66d0b81ac26
RobertPr <protasiukrobert@wp.pl>
2018-05-27 15:35:38 +0200

    linki

--- browarek/src/components/navbarUnlogged/NavbarUnLogged.js
+++ browarek/src/components/navbarUnlogged/NavbarUnLogged.js
@@ -14,18 +14,24 @@ class NavbarUnLogged extends React.Component {
                 <div className="fas fa-beer"></div>
                 <div id="logoText">Browarek</div>
                 <div id="navbarButtonsContainer">
+                <Link to="/login">
                     <div className="navbarButton">
                         <span className="fas fa-sign-in-alt"></span>
                         <span className="loginButtonText">Logowanie</span>
                     </div>
+                </Link>
+                <Link to="/register">
                     <div className="navbarButton">
                         <span className="fas fa-user-plus"></span>
                         <span className="loginButtonText">Rejestracja</span>
                     </div>
+                </Link>
+                <Link to="/beerSearch">
                     <div className="navbarButton">
                         <span className="fas fa-search"></span>
                         <span className="loginButtonText">Wyszukiwarka</span>
                     </div>
+                </Link>
                 </div>
             </div>
         );
